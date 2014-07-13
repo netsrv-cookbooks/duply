1486aa5290122ebff29f9ac0f3f35bbfe7419b04
Colin Woodcock <cwoodcock@netsrv-consulting.com>
2014-07-13 16:53:57 +0100

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of duply
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'duply'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures duply'
+long_description 'Installs/Configures duply'
+version          '0.1.0'
+
