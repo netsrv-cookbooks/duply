1347fa8eb511d001eee340d17f609a804cf1d0df
Colin Woodcock <cwoodcock@netsrv-consulting.com>
2014-07-17 07:55:15 +0100

    do not write empty globs

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'cwoodcock@netsrv-consulting.com'
 license          'Apache License, Version 2.0'
 description      'Installs/configures Duply and Duplicity for Amazon S3'
 long_description 'Installs/configures Duply and Duplicity for Amazon S3'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'yum-epel'
